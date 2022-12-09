89fa913df98d5e71da2ec2ffcb3d795053cda843
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-12-09 16:05:04 +0700

    Add project files.

---
+++ Exercise4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace Exercise4
+{
+    class Exercise4
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack();
+            st.Push('e');
+            st.Push('f');
+            st.Push('g');
+            Console.WriteLine("Stack = ");
+            foreach (char ch in st)
+            {
+                Console.WriteLine(ch + " ");
+            }
+        }
+    }
+}
